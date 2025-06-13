1ca2467470bb0c3faa0aaaacd0c3854965b0af11
= <=>
2025-06-13 21:31:48 +0600

    Setup Connections

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import logger from "./logger/logger.js";
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 7000;
+
+app.listen(port, () => logger.info(`Listening on port ${port}...`));
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => logger.info("Connected to database..."))
+  .catch((err) => logger.error(err));
