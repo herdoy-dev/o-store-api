fab089a882b0e84c7777d5dd6b2739d5670ad802
= <=>
2025-06-16 22:15:31 +0600

    Update Cors

--- config/cookieOptions.js
+++ config/cookieOptions.js
@@ -3,7 +3,10 @@ const cookieOptions = {
   secure: true,
   sameSite: "none",
   maxAge: 24 * 60 * 60 * 1000,
-  domain: process.env.NODE_ENV === "production" ? ".netlify.app" : "localhost",
+  domain:
+    process.env.NODE_ENV === "production"
+      ? "hk-e-store.netlify.app"
+      : undefined,
 };
 
 export default cookieOptions;
